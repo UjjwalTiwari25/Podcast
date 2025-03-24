b874dbe3e9c42cf206c0d3bb9147500248cd5be7
UjjwalTiwari25 <ujjwaltiwari153@gmail.com>
2025-03-24 11:07:52 +0530

    database connected

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 require("dotenv").config();
+require("./conn/conn");
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port : ${process.env.PORT}`);
--- backend/conn/conn.js
+++ backend/conn/conn.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
+
 const conn = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(`${process.env.MONGO_URI}`);
         console.log("Database connected");
     } catch (error) {
-        console.log(error);
+        console.log("Database connection error:", error);
     }
 };
+
+conn(); // Call the function to establish the connection
+
 module.exports = conn;
