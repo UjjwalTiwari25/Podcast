e9c2419c8bc8230c2b441b2b74fb8a6ea8ba2e74
UjjwalTiwari25 <ujjwaltiwari153@gmail.com>
2025-04-11 11:13:38 +0530

    cors updated

--- backend/app.js
+++ backend/app.js
@@ -1,32 +1,31 @@
 const express = require('express');
 const app = express();
 const cookieParser = require("cookie-parser");
-const userRoutes = require("./routes/user");
-const categoryRoutes = require("./routes/category");
-const podcastRoutes = require("./routes/podcast");
 const cors = require("cors");
-
-const corsOptions = {
-    origin: "https://podcast-gk2v.onrender.com",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-};
-app.use(cors(corsOptions));
-
 
 require("dotenv").config();
 require("./conn/conn");
 
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const podcastRoutes = require("./routes/podcast");
+
 // Parsing Middleware
 app.use(express.json());
 app.use(cookieParser());
+
+// CORS Config for Frontend + Localhost Dev
+app.use(cors({
+  origin: ['https://podcast-gk2v.onrender.com', 'http://localhost:5173'],
+  credentials: true
+}));
+app.options('*', cors()); // handle preflight
 
 // API Routes
 app.use("/api/v1", userRoutes);
 app.use("/api/v1", categoryRoutes);
 app.use("/api/v1", podcastRoutes);
 
-
 app.listen(process.env.PORT, () => {
-    console.log(`Server running on port : ${process.env.PORT}`);
-  });
+  console.log(`Server running on port : ${process.env.PORT}`);
+});
